9bf080595149131c6e1673fd7ce01ca6fb01cfb5
VVarbanov <vvarbanov99@gmail.com>
2018-05-12 17:49:07 +0300

    no message

--- Traders Server/src/main/java/com/diplomna/traders/repository/LaptopRepository.java
+++
@@ -1,7 +0,0 @@
-package com.diplomna.traders.repository;
-
-import com.diplomna.traders.Models.Laptop;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LaptopRepository extends CrudRepository<Laptop,Long> {
-}
--- Traders Server/src/main/java/com/diplomna/traders/repository/TVRepository.java
+++
@@ -1,7 +0,0 @@
-package com.diplomna.traders.repository;
-
-import com.diplomna.traders.Models.TV;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TVRepository extends CrudRepository<TV, Long> {
-}
