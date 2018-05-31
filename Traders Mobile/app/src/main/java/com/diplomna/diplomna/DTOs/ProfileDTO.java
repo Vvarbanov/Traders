c3a8d68cf58cda811b3f0bd6daf63a7a277256bd
VVarbanov <vvarbanov99@gmail.com>
2018-05-31 15:34:38 +0300

    no message

---
+++ Traders Mobile/app/src/main/java/com/diplomna/diplomna/DTOs/ProfileDTO.java
@@ -0,0 +1,12 @@
+package com.diplomna.diplomna.DTOs;
+
+public class ProfileDTO {
+
+
+    private String username;
+    private String email;
+    private String phone;
+    private AccountType accountType;
+
+
+}
