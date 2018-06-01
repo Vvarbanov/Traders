4810728d7029d3a8f7caecdd8d0963d672406b9f
VVarbanov <vvarbanov99@gmail.com>
2018-06-01 14:20:02 +0300

    no message

---
+++ Traders Server/src/main/java/com/diplomna/traders/dtos/ItemDTO.java
@@ -0,0 +1,63 @@
+package com.diplomna.traders.dtos;
+
+public class ItemDTO {
+
+    private String name;
+    private String description;
+    private double basePricePerUnit;
+    private String user;
+    private String subCategory;
+    private int quantity;
+    
+
+    public ItemDTO() { }
+    
+    public int getQuantity() {
+        return quantity;
+    }
+    
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    public String getSubCategory() {
+        return subCategory;
+    }
+
+    public void setSubCategory(String subCategory) {
+        this.subCategory = subCategory;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBasePricePerUnit() {
+        return basePricePerUnit;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setBasePricePerUnit(double basePricePerUnit) {
+        this.basePricePerUnit = basePricePerUnit;
+    }
+
+}
---
+++ Traders Server/src/main/java/com/diplomna/traders/models/Item.java
@@ -0,0 +1,98 @@
+package com.diplomna.traders.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "item_name")
+    private String name;
+
+    @Column(name = "item_base_price_per_unit")
+    private double basePricePerUnit;
+
+    @Column(name = "item_description")
+    @Size(max = 5000)
+    private String description;
+    
+    private int quantity;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "subcategory_id",nullable = false)
+    private SubCategory subCategory;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+    public Item(String name, double basePricePerUnit, String description, SubCategory subCategory, User user) {
+        this.name = name;
+        this.basePricePerUnit = basePricePerUnit;
+        this.description = description;
+        this.subCategory = subCategory;
+        this.user = user;
+    }
+
+    public Item() {}
+    
+    public int getQuantity() {
+        return quantity;
+    }
+    
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBasePricePerUnit() {
+        return basePricePerUnit;
+    }
+
+    public void setBasePricePerUnit(double basePricePerUnit) {
+        this.basePricePerUnit = basePricePerUnit;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public long getId() {
+        return id;
+    }
+
+    public SubCategory getSubCategory() {
+        return subCategory;
+    }
+
+    public void setSubCategory(SubCategory subCategory) {
+        this.subCategory = subCategory;
+    }
+}
